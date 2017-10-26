d044546c9f1ecb33a395a873cd42659b1e0d1644
Rahul M V <15it229.rahul@nitk.edu.in>
2017-10-26 21:40:23 +0530

    Added codes

---
+++ fuzzy_c_means.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import random
+import math
+
+def eucledian_dist(a,b):
+    ans = float(np.sqrt(np.sum((a-b)**2)))
+    return ans
+
+if __name__== '__main__':
+    df = pd.read_csv('/home/rahul/Desktop/SC/SPECTF_New.csv')
+    df = df.sample(frac=1)
+    #print df.head()
+    df = df.as_matrix()
+    print df.shape
+    no_of_clusters = 2
+    m = 2
+    rows = random.sample(range(0,df.shape[0]),no_of_clusters)
+    centers = []
+    for i in range(no_of_clusters):
+        centers.append(df[rows[i],:df.shape[1]-1])
+    centers = np.array(centers,dtype=np.float)
+    #print centers
+    membership_matrix = np.zeros((df.shape[0],no_of_clusters),dtype=np.float)
+
+    delta = 0.01
+    itr=0
+    flag=1
+
+    while itr<500 and flag==1:
+        print "--------------------------------------------------------------------------------------------"
+        for i in range(membership_matrix.shape[0]):
+            for j in range(membership_matrix.shape[1]):
+                numerator = eucledian_dist(df[i,:44],centers[j,:])
+                if numerator==0:
+                    membership_matrix[i,j] = 1
+                    for k in range(j):
+                        membership_matrix[i,k] = 0
+                    break
+                else:
+                    sumratios = 0
+                    for k in range(centers.shape[0]):
+                        dist = eucledian_dist(df[i,:44],centers[k,:])
+                        if dist==0:
+                            ratio = 0
+                        else:
+                            ratio = float(numerator)/dist
+                        ratio = math.pow(ratio,2.0/(m-1))
+                        sumratios += ratio
+                    membership_matrix[i,j] = 1/float(sumratios)
+
+        '''
+        for i in range(membership_matrix.shape[0]):
+            print np.sum(membership_matrix[i])
+        '''
+        #print centers
+        flag=0
+        for i in range(centers.shape[0]):
+            numerator=np.zeros(44)
+            denominator=0
+            for j in range(df.shape[0]):
+                #print df[j,:44].shape
+                numerator = numerator + (df[j,:44] * math.pow(membership_matrix[j,i],m))
+                denominator += math.pow(membership_matrix[j,i],m)
+            if all(i<delta for i in centers[i]-(numerator/denominator)) is False:
+                flag=1
+            centers[i] = numerator/denominator
+        itr+=1
+        print membership_matrix
+        print itr
+        print "--------------------------------------------------------------------------------------------"
---
+++ k_means.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import math
+import random
+
+if __name__== '__main__':
+    df = pd.read_csv('/home/rahul/Desktop/SC/SPECTF_New.csv')
+    #df = df.sample(frac=1)
+    #print df.head()
+    df = df.as_matrix()
+    no_of_clusters = 2
+    rows = random.sample(range(0,df.shape[0]),no_of_clusters)
+    #rows = [30,80]
+    centers = []
+    for i in range(no_of_clusters):
+        centers.append(df[rows[i],:df.shape[1]-1])
+    centers = np.array(centers,dtype=np.float)
+    clusters = np.zeros(no_of_clusters,dtype=np.object)
+
+    print centers
+
+    k=0
+    flag=1
+
+    while k<10 and flag==1:
+        for i in range(clusters.shape[0]):
+            clusters[i] = []
+
+        for i in range(df.shape[0]):
+            distances = []
+            for j in range(no_of_clusters):
+                a = df[i,:df.shape[1]-1]
+                b = centers[j]
+                distances.append(np.sqrt(np.sum((a-b)**2)))
+            #print distances
+            cluster_index = distances.index(min(distances))
+            clusters[cluster_index].append((i,df[i,44]))
+        flag=0
+        for i in range(clusters.shape[0]):
+            indices = clusters[i]
+            arr = np.zeros(44,dtype=np.float)
+            for j in range(len(indices)):
+                arr = arr + df[(indices[j])[0],:df.shape[1]-1]
+            arr = arr/len(indices)
+            if np.array_equal(arr,centers[i]) is False:
+                flag=1
+            centers[i] = arr
+        #print clusters.shape
+        k+=1
+        print "---------------------------------------------------------------------------------------------------------------"
+        print centers
+        print clusters
+        print k
